95c8e0d665616c8d14327477944b9c950463f59d
mksiq <maickelsiqueira@gmail.com>
2020-07-28 16:17:31 -0400

    first commit with initial design

---
+++ products.js
@@ -0,0 +1,38 @@
+var products = [
+    {
+        id: 1,
+        name: "Beyond Burger",
+        description: "A burger with taste so rich and texture so meaty, you won't believe it’s made from plants.",
+        brand: "Beyond Meat",
+        category:["hamburger",
+        "vegan"],
+        weigth: 226,
+        price: 10.90,
+        available: true,
+        discount: 0
+    }, 
+    {
+        id: 2,
+        name: "Beyond Beef",
+        description: "The freedom and versatibility to turn any recipe into a meaty plant-based masterpiece.",
+        brand: "Beyond Meat",
+        category:["beef",
+        "vegan"],
+        weigth: 453,
+        price: 15.90,
+        available: true,
+        discount: 0
+    },   
+    {
+        id: 3,
+        name: "Beyond Sausage",
+        description: "From grill to griddle to frying pan, plant-based sausage stuffed with delicious flavor and satisfying sizzle..",
+        brand: "Beyond Meat",
+        category:["sausage",
+        "vegan"],
+        weigth: 400,
+        price: 11.50,
+        available: true,
+        discount: 0
+    },   
+];
